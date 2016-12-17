2adaf848446860489c692ee70cd9c50bc55b9d18
Bartosz <prawo123@interia.pl>
2016-12-17 17:18:15 +0100

    initial commit

---
+++ js/menu.js
@@ -0,0 +1,39 @@
+(function () {
+    let items = document.querySelectorAll('.nav-item .id'),
+        subItems = document.querySelectorAll('.nav-item .sub-item'),
+        _lastIndex = null;
+
+    let displayItems = (e) => {
+        let itemsArray = makeArray(items),
+            subItemsArray = makeArray(subItems),
+            itemsTarget = e.target,
+            itemsIndex = itemsArray.indexOf(itemsTarget),
+            subItemsIndex = itemsIndex;
+
+        if (!subItemsArray[subItemsIndex].classList.contains('show-sub-item')) {
+            subItemsArray[subItemsIndex].classList.add('show-sub-item');
+            if (_lastIndex != null) {
+                subItemsArray[_lastIndex].classList.remove('show-sub-item');
+            }
+            _lastIndex = subItemsIndex;
+        } else {
+            subItemsArray[subItemsIndex].classList.remove('show-sub-item');
+        }
+    };
+
+    let init = () => {
+        for (let i = 0; i < items.length; i++) {
+            items[i].addEventListener('click', displayItems, false);
+        }
+    };
+
+    let makeArray = (items) => {
+        let array = [];
+        for (let i = 0; i < items.length; i++) {
+            array.push(items[i]);
+        }
+        return array;
+    };
+
+    init();
+})();
